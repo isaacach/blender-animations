ed06bc4362022e0b4c5cb1ce60d56ac6e508ef85
isaacach <124728947+isaacach@users.noreply.github.com>
2024-02-08 21:45:44 -0600

    added blender animation

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+'use-client'
+// eslint-disable-next-line no-unused-vars
+import Spline from '@splinetool/react-spline'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='animation-wrapper'>
+        <iframe className='animation' src="https://my.spline.design/untitled-f669e8840a1b3493a2084e2ea8e8851a/" width='100%'></iframe>
+      </div>
+    </>
+  )
+}
+
+export default App
